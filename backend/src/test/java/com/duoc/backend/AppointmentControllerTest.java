4d5ca3810d7d543d3ed9b0ac85abc7216ea31d7e
Andrea Rosero <andrea.rosero.p@gmail.com>
2026-05-08 21:07:33 -0400

    cambios en mockito por mockbean

--- backend/src/test/java/com/duoc/backend/AppointmentControllerTest.java
+++ backend/src/test/java/com/duoc/backend/AppointmentControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -50,10 +50,10 @@ class AppointmentControllerTest {
         .registerModule(new JavaTimeModule())
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-    @MockitoBean
+    @MockBean
     private AppointmentRepository appointmentRepository;
 
-    @MockitoBean
+    @MockBean
     private PatientRepository patientRepository;
 
     private Appointment buildAppointment(Integer patientId) {
